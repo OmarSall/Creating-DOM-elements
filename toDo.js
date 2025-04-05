5f769c8c2d42ff4aa85c0be5efc95b283da3753e
Omar Salloum <omarsalloum08@gmail.com>
2025-04-05 16:40:46 +0200

    To do app. Adding html structure and first css styling

---
+++ toDo.js
@@ -0,0 +1,10 @@
+const addTaskButton = document.getElementById("addTask");
+
+addTaskButton.addEventListener("click", () => {
+    const taskInput = document.getElementById("taskName");
+    const taskName = taskInput.ariaValueMax.trim();
+
+    if (taskName) {
+        
+    }
+});
