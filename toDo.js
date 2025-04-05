58649a61024b9fdd07dc06a87b5eb4cf65eef480
Omar Salloum <omarsalloum08@gmail.com>
2025-04-05 17:02:51 +0200

    add task functionality

--- toDo.js
+++ toDo.js
@@ -2,9 +2,28 @@ const addTaskButton = document.getElementById("addTask");
 
 addTaskButton.addEventListener("click", () => {
     const taskInput = document.getElementById("taskName");
-    const taskName = taskInput.ariaValueMax.trim();
+    const taskName = taskInput.value.trim();
 
     if (taskName) {
-        
+        addTaskToColumn("toDo", taskName);
+        taskInput.value = "";
     }
 });
+
+function addTaskToColumn(columnId, taskName) {
+    const column = document.getElementById(columnId);
+    const taskElement = document.createElement("div");
+    taskElement.classList.add("task");
+    taskElement.innerHTML = `
+    ${taskName}
+    <div>
+        <button class="delete" title="Delete" onclick="deleteTask(event)">🗑️</button>
+    </div>
+    `
+    column.appendChild(taskElement);
+}
+
+function deleteTask(event) {
+    const taskElement = event.target.closest(".task");
+    taskElement.remove();
+}
